4b420da23de9b57d8778e9cc48e953aa977dde1e
邓伟键 <emmmvkdeng@gmail.com>
2026-01-18 23:47:07 +0800

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./App.tsx",
-    "./index.tsx",
+    "./**/*.{js,ts,jsx,tsx}",
+    "!./node_modules/**",
+    "!./dist/**",
+    "!./apps/portal-backend/**" 
   ],
   theme: {
     extend: {
