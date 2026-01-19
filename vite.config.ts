fff1dde969f0f7fb51988b0cd2d1992c021e7965
邓伟键 <emmmvkdeng@gmail.com>
2026-01-19 14:02:07 +0800

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const base = (env.VITE_BASE || '/').trim() || '/';
     return {
+      base,
       server: {
         port: 3000,
         host: '0.0.0.0',
